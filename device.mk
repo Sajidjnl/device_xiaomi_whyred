8ab53cdd8867ca6c848aab960f277c82ee57149d
mdeejay <mdjrussia@gmail.com>
2018-06-19 13:21:18 +0200

    whyred: build livedisplay

--- device.mk
+++ device.mk
@@ -27,7 +27,8 @@ $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 	
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -293,6 +294,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
+	
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
